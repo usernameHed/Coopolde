9f9138292861335e9b88f99300f53e411371c3c1
DESKTOP-20HJOKH\Hed <ugo@belfiore.fr>
2018-10-14 19:47:37 +0200

    ok CCC done

---
+++ Coopolde/Assets/_Scripts/Game/AlwaysFollow.cs
@@ -0,0 +1,23 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlwaysFollow : MonoBehaviour
+{
+    [FoldoutGroup("GamePlay"), Tooltip("Le smooth de la caméra"), SerializeField]
+    private float smoothTime = 0.2f;
+
+    [SerializeField]
+    private Transform targetToFollow;
+
+    private Vector3 currentVelocity;
+
+    private void FixedUpdate()
+    {
+        // Move to desired position
+        Vector3 desiredPosition = new Vector3(targetToFollow.position.x, transform.position.y, targetToFollow.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref currentVelocity, smoothTime);
+        //posLisener = transform.position;    //change listenerPosition
+    }
+}
