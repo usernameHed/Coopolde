43c1c4c623281f22a183a4ac50915a0f40fb587d
DESKTOP-20HJOKH\Hed <ugo@belfiore.fr>
2018-10-15 18:26:50 +0200

    ok Crawler

---
+++ Coopolde/Assets/_Scripts/Game/IAs/Crawler/CrawlerController.cs
@@ -0,0 +1,92 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrawlerController : EntityController, IKillable
+{
+    [FoldoutGroup("GamePlay"), SerializeField]
+    private float timeOfDeath = 0.5f;
+
+    [SerializeField]
+    public GameObject refToFollow = null;
+
+    [SerializeField, ReadOnly]
+    private bool isDying = false;
+    public bool IsDying { get { return (isDying); } }
+
+    private Vector3 dirCura = new Vector3(0, 0, 0);
+
+
+    private void Start()
+    {
+        base.Init();
+        Init();
+    }
+
+    public override void Init()
+    {
+        refToFollow = CoopoldeManager.Instance.GetTarget();
+
+        rb.transform.localPosition = Vector3.zero;
+        isDying = false;
+    }
+
+    /// <summary>
+    /// change direction of the script who turn
+    /// </summary>
+    private void TurnPlayer()
+    {
+        dirCura = refToFollow.transform.position - rb.transform.position;
+        entityTurn.SetDirection(new Vector2(dirCura.x, dirCura.z), false);
+    }
+
+    /// <summary>
+    /// move in physics, according to input of player
+    /// </summary>
+    private void MovePlayer()
+    {
+        playerMove.Move(rb.transform.forward);
+    }
+
+    /// <summary>
+    /// handle input
+    /// </summary>
+    private void Update()
+    {
+        if (!enabledScript)
+            return;
+        TurnPlayer();
+    }
+
+    /// <summary>
+    /// handle move physics
+    /// </summary>
+    private void FixedUpdate()
+    {
+        if (!enabledScript || isDying)
+            return;
+        MovePlayer();
+    }
+
+    [Button]
+    public void Kill()
+    {
+        if (isDying)
+            return;
+
+        isDying = true;
+        entityTurn.SetDirection(dirCura, true);
+
+        StartCoroutine(RealyKill());
+    }
+
+    private IEnumerator RealyKill()
+    {
+        yield return new WaitForSeconds(timeOfDeath);
+
+
+        //ObjectsPooler.Instance.SpawnFromPool(GameData.PoolTag.DeadCuca, rb.transform.position, rb.transform.rotation, ObjectsPooler.Instance.transform);
+        Destroy(gameObject);
+    }
+}
