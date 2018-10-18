334918e1d3613cbb1c0a3bbcef6e32e03ca5cdbc
DESKTOP-20HJOKH\Hed <ugo@belfiore.fr>
2018-10-18 13:38:15 +0200

    add bonus

---
+++ Coopolde/Assets/_Scripts/Game/Bonnus.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bonnus : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(GameData.Layers.Player.ToString()))
+        {
+            SoundManager.GetSingleton.PlaySound("Bonus");
+            SpawnerBonus.Instance.Spawn();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Coopolde/Assets/_Scripts/Game/SpawnerBonus.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerBonus : SingletonMono<SpawnerBonus>
+{
+    [SerializeField]
+    private float rangeRadius = 3f;
+
+    [SerializeField]
+    private List<IsOnCamera> spawnPoints;
+
+    [SerializeField]
+    private Transform parentSpawn;
+    [SerializeField]
+    private Transform parentBonus;
+    [SerializeField]
+    private GameObject prefabsBonus;
+
+    private void OnEnable()
+    {
+        EventManager.StartListening(GameData.Event.GameOver, GameOver);
+    }
+
+    private void Start()
+    {
+        Spawn();
+    }
+
+    public void Spawn()
+    {
+        CoopoldeManager.Instance.difficulty++;
+
+        List<IsOnCamera> spawns = new List<IsOnCamera>();
+        for (int i = 0; i < spawnPoints.Count; i++)
+        {
+            if (!spawnPoints[i].isOnScreen)
+                spawns.Add(spawnPoints[i]);
+        }
+        int indexRandom = Random.Range(0, spawns.Count);
+        Vector3 pos = spawns[indexRandom].transform.position;
+        pos.x = (pos.x - rangeRadius) + Random.Range(0, rangeRadius * 2);
+        pos.z = (pos.z - rangeRadius) + Random.Range(0, rangeRadius * 2);
+        GameObject bonus = Instantiate(prefabsBonus, pos, Quaternion.identity, parentBonus);
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("game over !!");
+        gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        EventManager.StopListening(GameData.Event.GameOver, GameOver);
+    }
+}
